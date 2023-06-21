e052efe64edc0c3e505b82937fea943f13326e9f
leider <leideryesidmm@gmail.com>
2023-06-21 18:52:53 -0500

    Se conecto enlistar pacientes, agregar pacientes, agregar prescripciones

--- js/pacientes.js
+++ js/pacientes.js
@@ -1,27 +1,11 @@
-let listarPacientes= async()=>{
-  const peticion= await fetch("http://localhost:8081/Medico/findAllPacientes",{
-    method:"GET",
-    headers:{
-      "Accept":"application/json",
-      "Content-Type":"application/json"
-    }
-  });
-  const pacients=await peticion.json();
-  console.log(pacients);}
-  listarPacientes();
 
-document.addEventListener('DOMContentLoaded', function() {
-
-
-
-  
+  document.addEventListener('DOMContentLoaded', function() {
     // Obtener referencia a los elementos del DOM
     var filtroInput = document.getElementById('filtro-paciente');
     var btnFiltrar = document.getElementById('btn-filtrar');
     var tablaPacientes = document.getElementById('tabla-pacientes-body');
     var btnNuevo = document.getElementById('btn-nuevo');
-  
-
+    
     // Función para filtrar los pacientes por nombre
     function filtrarPacientes() {
       var filtro = filtroInput.value.toLowerCase();
@@ -46,21 +30,19 @@ document.addEventListener('DOMContentLoaded', function() {
         var row = document.createElement('tr');
         var nombresCell = document.createElement('td');
         nombresCell.textContent = paciente.nombre;
-        var apellidosCell = document.createElement('td');
-        apellidosCell.textContent = paciente.apellido;
         var ultimaCitaCell = document.createElement('td');
         ultimaCitaCell.textContent = paciente.ultimaCita;
         var accionesCell = document.createElement('td');
         var verButton = document.createElement('button');
         verButton.textContent = 'Ver';
         var eliminarButton = document.createElement('button');
-        eliminarButton.textContent = 'Eliminar';
+        eliminarButton.textContent = 'Inhabilitar';
+        
   
         accionesCell.appendChild(verButton);
         accionesCell.appendChild(eliminarButton);
   
         row.appendChild(nombresCell);
-        row.appendChild(apellidosCell);
         row.appendChild(ultimaCitaCell);
         row.appendChild(accionesCell);
   
@@ -68,44 +50,27 @@ document.addEventListener('DOMContentLoaded', function() {
       });
     }
     
-// Agregar evento de clic a los botones de ver
-   tablaPacientes.addEventListener('click', function(event) {
-    if (event.target.matches('button[data-id]')) {
-      var pacienteId = event.target.getAttribute('data-id');
-      verPaciente(pacienteId);
-    }
-  });
-  
-  // Función para ver los detalles de un paciente
-  function verPaciente(pacienteId) {
-    window.location.href = 'info-pacientes.html?id=' + pacienteId;
-  }
-  // Función para ocultar un paciente
-function ocultarPaciente(pacienteId) {
-    // Obtener la fila del paciente por su identificador
-    var filaPaciente = document.getElementById('paciente-' + pacienteId);
-  
-    // Ocultar la fila estableciendo la propiedad display en 'none'
-    filaPaciente.style.display = 'none';
-  }
-  
-  // Agregar evento de clic al botón de eliminar
-  btnEliminar.addEventListener('click', function() {
-    // Obtener el identificador del paciente
-    var pacienteId = obtenerPacienteIdDesdeURL();
-  
-    // Ocultar el paciente en lugar de eliminarlo
-    ocultarPaciente(pacienteId);
-  });
-  
-    // Agregar evento de clic al botón de filtrar
+    // Agregar botón de filtrar
     btnFiltrar.addEventListener('click', filtrarPacientes);
   
-    // Agregar evento de clic al botón nuevo para redireccionar a agg-paciente.html
+    // Redireccionar a agg-paciente.html para agregar el paciente 
     btnNuevo.addEventListener('click', function() {
       window.location.href = 'agg-pacientes.html';
     });
   
-    // Mostrar todos los pacientes al cargar la página
-    actualizarTabla(pacientesData);
+    // Obtener los datos de los pacientes al cargar la página
+    async function obtenerPacientes() {
+      const peticion = await fetch("http://localhost:8081/Medico/findAllPacientes", {
+        method: "GET",
+        headers: {
+          "Accept": "application/json",
+          "Content-Type": "application/json"
+        }
+      });
+      const pacientes = await peticion.json();
+      // Actualizar la tabla con los pacientes obtenidos de la base de datos
+      actualizarTabla(pacientes);
+    }
+  
+    obtenerPacientes();
   });
