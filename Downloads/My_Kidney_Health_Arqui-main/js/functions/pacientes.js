0c4d1520cb99a8ef77cbeb465962c57758af22c8
JheynerLobo21 <jheyner21lobo@gmail.com>
2023-08-02 16:16:00 -0500

    agregar paciente

---
+++ Downloads/My_Kidney_Health_Arqui-main/js/functions/pacientes.js
@@ -0,0 +1,51 @@
+let pacientesTratados = async () => {
+  try {
+    // Call listarPacientes and await its result
+    const pacientes = await listarPacientes();
+
+    let msg = "";
+    if (pacientes != null && pacientes.length > 0) {
+      msg += '<br>' +
+        '<table class="pacientes">' +
+        '<tr>' +
+        '<th>Nombre</th>' +
+        '<th>Cédula</th>' +
+        '<th>Acciones</th>' +
+        '</tr>';
+
+      pacientes.forEach((paciente) => {
+        msg +=
+          '<tr>' +
+          '<td>' + paciente.nombre + '</td>' +
+          '<td>' + paciente.cedula + '</td>' +
+          '<td>' +
+          '<a href="info-pacientes.html?cedula=' + paciente.cedula + '" type="button">' +
+          '<img src="../img/logo.jpg" class="actualizar"/>' +
+          '</a>' +
+          '<a href="InhabilitarPaciente.html?cedula=' + paciente.cedula + '" type="button">' +
+          '<img src="../img/nefrologo.png" class="actualizar"/>' +
+          '</a>' +
+          '</td>' +
+          '</tr>';
+      });
+    } else {
+      msg += '<br>' +
+        '<table class="pacientes">' +
+        '<tr>' +
+        '<th>Nombre</th>' +
+        '<th>Cédula</th>' +
+        '<th>Acciones</th>' +
+        '</tr>' +
+        '<tr>' +
+        '<td colspan="3">' + "No hay pacientes Registrados" + '</td>' +
+        '</tr>';
+    }
+    msg += '</table>';
+    document.getElementById("pacientes").innerHTML = msg;
+  } catch (error) {
+    console.error("Error in pacientesTratados:", error);
+  }
+};
+
+// Call the pacientesTratados function to populate the table
+pacientesTratados();
---
+++ Downloads/My_Kidney_Health_Arqui-main/js/pacientes.js
@@ -0,0 +1,85 @@
+
+  document.addEventListener('DOMContentLoaded', function() {
+    // Obtener referencia a los elementos del DOM
+    var filtroInput = document.getElementById('filtro-paciente');
+    var btnFiltrar = document.getElementById('btn-filtrar');
+    var tablaPacientes = document.getElementById('tabla-pacientes-body');
+    var btnNuevo = document.getElementById('btn-nuevo');
+    
+    // Función para filtrar los pacientes por nombre
+    function filtrarPacientes() {
+      var filtro = filtroInput.value.toLowerCase();
+  
+      // Filtrar los pacientes según el valor del filtro
+      var pacientesFiltrados = pacientesData.filter(function(paciente) {
+        var nombreCompleto = paciente.nombre.toLowerCase() + ' ' + paciente.apellido.toLowerCase();
+        return nombreCompleto.indexOf(filtro) !== -1;
+      });
+  
+      // Actualizar la tabla con los pacientes filtrados
+      actualizarTabla(pacientesFiltrados);
+    }
+  
+    // Función para actualizar la tabla de pacientes
+    function actualizarTabla(pacientes) {
+      // Limpiar el contenido actual de la tabla
+      tablaPacientes.innerHTML = '';
+  
+      // Generar filas de la tabla con los pacientes
+      pacientes.forEach(function(paciente) {
+        var row = document.createElement('tr');
+        var nombresCell = document.createElement('td');
+        nombresCell.textContent = paciente.nombre;
+        nombresCell.style.textAlign = 'center';
+        var ultimaCitaCell = document.createElement('td');
+        ultimaCitaCell.textContent = paciente.cedula;
+        ultimaCitaCell.style.textAlign = 'center';
+        var accionesCell = document.createElement('td');
+        accionesCell.style.textAlign = 'center';
+        var verButton = document.createElement('button');
+        verButton.textContent = 'Ver';
+        verButton.style.textAlign = 'center';
+        var eliminarButton = document.createElement('button');
+        eliminarButton.textContent = 'Inhabilitar';
+        eliminarButton.style.marginLeft='10px'
+        eliminarButton.style.textAlign = 'center';
+        var editarButton = document.createElement('button');
+        editarButton.textContent = 'Editar';
+        editarButton.style.marginLeft='10px'
+        editarButton.style.textAlign = 'center';
+  
+        accionesCell.appendChild(verButton);
+        accionesCell.appendChild(eliminarButton);
+  
+        row.appendChild(nombresCell);
+        row.appendChild(ultimaCitaCell);
+        row.appendChild(accionesCell);
+  
+        tablaPacientes.appendChild(row);
+      });
+    }
+    
+    // Agregar botón de filtrar
+    btnFiltrar.addEventListener('click', filtrarPacientes);
+  
+    // Redireccionar a agg-paciente.html para agregar el paciente 
+    btnNuevo.addEventListener('click', function() {
+      window.location.href = 'agg-pacientes.html';
+    });
+  
+    // Obtener los datos de los pacientes al cargar la página
+    async function obtenerPacientes() {
+      const peticion = await fetch("http://localhost:8081/Medico/findAllPacientes", {
+        method: "GET",
+        headers: {
+          "Accept": "application/json",
+          "Content-Type": "application/json"
+        }
+      });
+      const pacientes = await peticion.json();
+      // Actualizar la tabla con los pacientes obtenidos de la base de datos
+      actualizarTabla(pacientes);
+    }
+  
+    obtenerPacientes();
+  });
